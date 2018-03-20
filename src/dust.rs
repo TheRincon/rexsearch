514727718a0ce01c37144deaf38af15f3a6f529e
daniel-whitfield-spotcap <daniel.whitfield@spotcap.com>
2018-03-20 13:18:40 +0100

    DUST now working

--- src/dust.rs
+++ src/dust.rs
@@ -1,17 +1,15 @@
 const DUST_LEVEL: i32 = 20;  // original was 20
 const DUST_WORD: i32 = 3;
 const DUST_WINDOW: i32 = 64; // original is 64
-const DUST_WINDOW_2: i32 = 32;
+const DUST_WINDOW_2: i32 = DUST_WINDOW >> 1;
 const WORD_COUNT: i32 = 1 << ( DUST_WORD << 1 );
 static BIT_MASK: i32 = WORD_COUNT - 1;
 
-fn wo(len: i32, seq: &[char], begin: i32, end: i32) -> (i32, i32, i32) {
+fn wo(len: i32, m: &mut [char], beg: &mut i32, end: &mut i32) -> i32 {
 
-    let l1: i32 = len - DUST_WORD + 1 - 5; //    - 4 ?
-    if l1 < 0 {
-        return (0, 0, 0);
-    }
-    let g = [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+    let l1 = len - DUST_WORD + 1 - 5;
+    if (l1 < 0) { return 0 };
+    let chrmap = [0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
         0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
         0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
         0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
@@ -27,89 +25,75 @@ fn wo(len: i32, seq: &[char], begin: i32, end: i32) -> (i32, i32, i32) {
         0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
         0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
         0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0];
-    let mut counts = vec![0;WORD_COUNT as usize];
-    let mut words = vec![0;DUST_WINDOW as usize];
-    let mut word: i32 = 0;
     let mut bestv: i32 = 0;
+    let mut besti: i32 = 0;
     let mut bestj: i32 = 0;
-    let mut bestk: i32 = 0;
-    let mut a = begin;
-    let mut b = end;
+    let mut  words = [0; 64];
+    let mut word: i32 = 0;
 
-    for i in 0..len {
+    for j in 0..len {
         word <<= 2;
-        //word |= match seq[i as usize] {
-        //    'T' | 't' | 'U' | 'u' => 3,
-        //    'G' | 'g'             => 2,
-        //    'T' | 't'             => 1,
-        //    _                     => 0
-        //};
-        word |= g[seq[i as usize] as usize];
-        // println!("word here is {:?}, ", word);
-        words[i as usize] = word & BIT_MASK;
-        // println!("{:?}", words[i as usize]);
+        word |= chrmap[m[j as usize] as usize];
+        words[j as usize] = word & BIT_MASK;
     }
-    // println!("words array is {:?}", words);
-    for j in 0..l1 {
-        let mut sum = 0;
-        for k in 2 .. (len-j) {
-            let p = j;
-            let q = k;
-            word = words[j as usize + k as usize];
-            let c: i32 = counts[word as usize];
-            // println!("{:?}", c);
+
+    for i in 0..l1 {
+        let mut counts = [0; 64];
+        let mut sum: i32 = 0;
+
+        for j in 2 .. len - i {
+            word = words[i as usize + j as usize];
+            let mut c = counts[word as usize];
             if c != 0 {
                 sum += c;
-                let mut v: i32 = 10 * sum / k;
-                // println!("{:?}", v);
-                if v > bestv {
-                    bestv = v;
-                    bestj = j;
-                    bestk = k;
-                }
+                let mut v = 10 * sum / j;
+                if (v > bestv)
+                    {
+                        bestv = v;
+                        besti = i;
+                        bestj = j;
+                    }
             }
             counts[word as usize] += 1;
         }
     }
-    a = bestj;
-    b = bestj + bestk;
-    println!("end of array is {:?}", b);
-    return (bestv, a, b);
+    let x = besti;
+    let y = bestj;
+    *beg = x;
+    *end = x + y;
+    return bestv;
 }
 
-pub fn duster(seq: &str, hardmask: bool) -> Vec<char> {
+pub fn dust(m: &mut [char], len: i32, hardmask: bool) {
 
-    let n = seq.len() as i32;
     let mut a: i32 = 0;
     let mut b: i32 = 0;
-
-    let mut ss: Vec<char> = seq.to_owned().clone().chars().collect();
-    // let s = ss.clone();
-    if !hardmask {
-        seq.to_uppercase();
+    let mut s = ['m'; 1020];
+    s.clone_from_slice(&m);
+    if (!hardmask) {
+        /* convert sequence to upper case unless hardmask in effect */
+        for i in 0..len { // m[i] = toupper(m[i])
+        };
+        // m[len] = 0;  // what is this?
     }
-
-    let mut i: i32 = 0;
-    while i < n {
-        let mut lm = if (n > (i + DUST_WINDOW)) { DUST_WINDOW } else { n - i };
-        let za = a;
-        let zb = b;
-        let (v, a, b) = wo(lm, &ss[i as usize..], za, zb);
-        println!("{:?}", &ss[i as usize..]);
-        if v > DUST_LEVEL {
+    let mut i = 0;
+    while i < len {
+        let mut l = if (len > i + DUST_WINDOW) { DUST_WINDOW } else { len-i };
+        let mut v = wo(l, &mut s[i as usize..], &mut a, &mut b);
+        if (v > DUST_LEVEL) {
             if hardmask {
-                // println!("{:?},{:?}",a,b);
-                for j in (a + 1)..(b + 1) {
-                    ss[j as usize] = 'N';
+                for j in (a+i) as usize..(b + i + 1 ) as usize {
+                    m[j as usize] = 'N';
                 }
             } else {
-                // convert ss range a+1 .. b+1 to lowercase. Somehow.
+                for j in (a + i)..(b + i + 1) {
+                    // let x = m[j as usize]; TODO
+                    // m[j as usize] = 'n';
+                    // m[j] = s[j] | 0x20;
+                }
             }
-            if b < DUST_WINDOW_2 {
-                i += DUST_WINDOW_2 - b;
-            }
+            if (b < DUST_WINDOW_2) { i += DUST_WINDOW_2 - b };
         }
         i += DUST_WINDOW_2;
     }
-    ss
 }
